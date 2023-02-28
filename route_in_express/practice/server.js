822fcd50e704a0b119ef47a7df12642ea97ae7ff
pradeep <pksingh0315@gmail.com>
2023-02-28 12:21:01 +0530

    first commit

---
+++ route_in_express/practice/server.js
@@ -0,0 +1,20 @@
+var express=require('express')
+var app=express();
+
+app.get('/users',(req,res)=>{
+    console.log(req.query);
+    res.json(req.query)
+    
+
+})
+app.get('/users/:username',(req,res,)=>{
+ var name=req.params.username
+ res.send(name)
+})
+app.post('/users',(req,res)=>{
+
+})
+
+app.listen(3000,()=>{
+    console.log('server is running at port 3K');
+})
