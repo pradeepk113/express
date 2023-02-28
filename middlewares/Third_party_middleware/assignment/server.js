ffdc71fcd7cd96f5e814f6f56bfd126c671fb24b
pradeep <pksingh0315@gmail.com>
2023-02-28 10:02:43 +0530

    second

---
+++ middlewares/Third_party_middleware/assignment/server.js
@@ -0,0 +1,28 @@
+var express = require('express')
+var cookieParser=require('cookie-parser')
+var logger=require('morgan')
+
+var app=express();
+app.use(cookieParser())
+
+app.use(logger('tiny'))
+
+app.use(express.static(__dirname + "/public"))
+ 
+app.use((req,res,next)=>{
+    console.log(req.cookies);
+    next()
+})
+app.use('/about',(req,res,next)=>{
+    res.cookie('username','Pradeep')
+    res.end('about page')
+    
+})
+
+
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname+ "/index.html")
+})
+app.listen(5000,()=>{
+    console.log("server is running at port 5K");
+})
